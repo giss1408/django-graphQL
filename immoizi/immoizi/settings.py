67efdecbc00007430e6cb89d83ae3373373a4518
Stanislas Regisse <kouabre.regisse@dormakaba.com>
2022-09-23 13:04:15 +0200

    Remove secret key from commit

--- immoizi/immoizi/settings.py
+++ immoizi/immoizi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m#=3^qw$40jynfq)nkvy82rzeq76dqymxyh5-&p)akrqk&2=%h'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
